c345b3cc8f7514bf02c2ded21823fe0078c9f6ae
Mojtaba Mohammadi <mojtaba@MacBook-Pro.local>
2020-10-24 00:45:53 +0330

    a1

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from google.auth.transport.requests import Request
 import pandas as pd
 
 
-def read_data_from_gsheet(SPREADSHEET_ID, RANGE_NAME):
+def read_data_from_gsheet(spreadSheetID, rangeName):
     # If modifying these scopes, delete the file token.pickle.
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
     # The ID and range of a sample spreadsheet.
-    SAMPLE_SPREADSHEET_ID = SPREADSHEET_ID
-    SAMPLE_RANGE_NAME = RANGE_NAME
+    SAMPLE_SPREADSHEET_ID = spreadSheetID
+    SAMPLE_RANGE_NAME = rangeName
 
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
@@ -48,10 +48,29 @@ def read_data_from_gsheet(SPREADSHEET_ID, RANGE_NAME):
     if not values:
         print('No data found.')
     else:
-        df = pd.DataFrame(values[1:], columns=values[0])
-        return df
+        dataframe = pd.DataFrame(values[1:], columns=values[0])
+        return dataframe
 
 
-df = read_data_from_gsheet('1n329f-UfjCc71QrxxAOkVisRXZKXqL-iq9mm_sedB1A', 'ARTs!A1:Q10')
+# Reading data from google sheet
+spreadSheetID = '1n329f-UfjCc71QrxxAOkVisRXZKXqL-iq9mm_sedB1A'
+rangeName = 'ARTs!A1:Q10'
+df = read_data_from_gsheet(spreadSheetID, rangeName)
 
-print(df)
+# Configuration
+chunkSize = 20
+mainCol = ['sku', 'meta:_sku', 'post_title', 'post_status', 'post_type', 'Type: product_type',
+           'tax:product_type', 'tax:product_cat', 'tax:product_tag', 'regular_price', 'meta:_price',
+           'sale_price', 'manage_stock', 'stock_status', 'images', 'attribute:pa_size', 'attribute_default:pa_size',
+           'attribute_data: pa_size', 'attribute:pa_sex', 'attribute_default:pa_sex', 'attribute_data: pa_sex',
+           'attribute:pa_color', 'attribute_data: pa_color', 'attribute:pa_material', 'attribute_data:pa_material',
+           'attribute:pa_print', 'attribute_data:pa_print', 'attribute:pa_collar', 'attribute_data:pa_collar',
+           'attribute:pa_wash', 'attribute_data:pa_wash', 'meta:_stock_status', 'meta:_wc_average_rating',
+           'meta:_wc_rating_count', 'post_content', 'post_excerpt']
+
+mainSheet = []
+for artCode in df['ArtCode']:
+
+    # Creating T-shirts
+    mainSheet.extend(mainCol)
+
