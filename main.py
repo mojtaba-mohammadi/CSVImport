f02468c7ae3db0177bb63bf6eba87f8424dafe4f
mojtaba-mohammadi <Mojtaba.mohammadi.095@gmail.com>
2020-10-26 01:36:39 +0330

    4th

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import csv
 import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import pandas as pd
+import random
 
 
 def read_data_from_gsheet(spreadSheetID, rangeName):
@@ -51,68 +53,233 @@ def read_data_from_gsheet(spreadSheetID, rangeName):
         return dataframe
 
 
-# Creating T-shirts
-
-# Reading data from google sheet
-spreadSheetID = '1n329f-UfjCc71QrxxAOkVisRXZKXqL-iq9mm_sedB1A'
-rangeName = 'ARTs!A1:Q10'
-df = read_data_from_gsheet(spreadSheetID, rangeName)
-columns = df.columns
-
-# Configuration
-chunkSize = 20
 mainCol = ['sku', 'meta:_sku', 'post_title', 'post_status', 'post_type', 'Type: product_type',
-           'tax:product_type', 'tax:product_cat', 'tax:product_tag', 'regular_price', 'meta:_price',
-           'sale_price', 'manage_stock', 'stock_status', 'images', 'attribute:pa_size', 'attribute_default:pa_size',
-           'attribute_data: pa_size', 'attribute:pa_sex', 'attribute_default:pa_sex', 'attribute_data: pa_sex',
-           'attribute:pa_color', 'attribute_data: pa_color', 'attribute:pa_material', 'attribute_data:pa_material',
-           'attribute:pa_print', 'attribute_data:pa_print', 'attribute:pa_collar', 'attribute_data:pa_collar',
-           'attribute:pa_wash', 'attribute_data:pa_wash', 'meta:_stock_status', 'meta:_wc_average_rating',
-           'meta:_wc_rating_count', 'post_content', 'post_excerpt']
-
-a = ['MainCat', 'Category', 'ArtCode', 'Title', 'Hashtag', 'Tshirt-سفید',
-     'Tshirt-مشکی', 'Tshirt-خاکستری', 'Tshirt-آبی', 'Tshirt-زرشکی',
-     'Tshirt-زرد', 'Tshirt-سبز', 'Hoodie-ملانژ', 'Hoodie-مشکی',
-     'Hoodie-زرشکی', 'Hoodie-نارنجی', 'Hoodie-سرمه‌ای']
+           'tax:product_type', 'tax:product_cat', 'tax:product_tag', 'regular_price', 'sale_price',
+           'meta:_price', 'manage_stock', 'stock_status', 'images',
+           'attribute:pa_style', 'attribute_default:pa_style', 'attribute_data:pa_style',
+           'attribute:pa_sex', 'attribute_default:pa_sex', 'attribute_data:pa_sex',
+           'attribute:pa_color', 'attribute_default:pa_color', 'attribute_data:pa_color',
+           'attribute:pa_size', 'attribute_default:pa_size', 'attribute_data:pa_size',
+           'attribute:pa_material', 'attribute_data:pa_material',
+           'attribute:pa_print', 'attribute_data:pa_print',
+           'attribute:pa_wash', 'attribute_data:pa_wash',
+           'attribute:pa_default_style', 'attribute_data:pa_default_style',
+           'meta:_wc_average_rating', 'meta:_wc_rating_count', 'meta:_wc_review_count',
+           'meta:_stock_status', 'post_content', 'post_excerpt']
 
 mainSheet = pd.DataFrame([], columns=mainCol)
 
-tshirtColor = ['سفید', 'مشکی', 'خاکستری', 'آبی', 'زرشکی', 'زرد', 'سبز']
+tshirtColor = ['color1', 'color2', 'color3', 'color4', 'color5', 'color6', 'color7']
 tshirtSize = ['M', 'L', 'XL', '2XL', '3XL']
-hoodieColor = ['ملانژ', 'مشکی', 'زرشکی', 'نارنجی', 'سرمه‌ای']
+hoodieColor = ['color8', 'color2', 'color5', 'color9', 'color10']
 hoodieSize = ['M', 'L', 'XL']
 colorIndex = pd.Series(
-    ['white', 'black', 'charcoal', 'blue', 'burgundy', 'yellow', 'greygreen', 'melange', 'orange', 'navy'],
-    index=['سفید', 'مشکی', 'خاکستری', 'آبی', 'زرشکی', 'زرد', 'سبز', 'ملانژ', 'نارنجی', 'سرمه‌ای']
+    ['سفید', 'مشکی', 'خاکستری', 'آبی', 'زرشکی', 'زرد', 'سبز خاکی', 'ملانژ', 'نارنجی', 'سرمه‌ای'],
+    index=['color1', 'color2', 'color3', 'color4', 'color5', 'color6', 'color7', 'color8', 'color9', 'color10']
 )
+
+# Configuration
+chunkSize = 100
+artCount = 657
+spreadSheetID = '1n329f-UfjCc71QrxxAOkVisRXZKXqL-iq9mm_sedB1A'
+# Reading data from google sheet
+rangeName = 'ARTs!A1' + ':Q' + str(artCount + 1)
+df = read_data_from_gsheet(spreadSheetID, rangeName)
 
 i = 0
 for artCode in df['ArtCode']:
     # create T-shirts
-    sku = df.loc[[i], ['ArtCode']] + '-TS'
-    post_title = df.loc[[i], ['Title']]
+    sku = df.loc[i]['ArtCode'] + '-TS'
+    post_title = df.loc[i]['Title']
     post_status = 'publish'
     post_type = 'product'
     product_type = 'variable'
-    product_cat = df.loc[[i], ['Category']]
+    product_cat = df.loc[i]['Category']
     product_cat = product_cat.replace(" - ", ">")
     product_cat = product_cat.replace("- ", ">")
     product_cat = product_cat.replace(" -", ">")
     product_cat = product_cat.replace("-", ">")
-    product_tag = df.loc[[i], ['Hashtag']]
+    product_tag = df.loc[i]['Hashtag']
     regular_price = 120000
     manage_stock = 'no'
     stock_status = 'instock'
 
-    images = sku + '-' + colorIndex[0] + '.jpg'
-    images = images + '|' + sku + '-' + colorIndex[0] + '-men' + '.jpg'
-    images = images + '|' + sku + '-' + colorIndex[0] + '-women' + '.jpg'
-    for color in tshirtColor[1:]:
-        images = images + '|' + sku + '-' + colorIndex[color] + '.jpg'
-        images = images + '|' + sku + '-' + colorIndex[color] + '-men' + '.jpg'
-        images = images + '|' + sku + '-' + colorIndex[color] + '-women' + '.jpg'
+    j = 0
+    images = ""
+    for color in tshirtColor:
+        if int(df.loc[i]['Tshirt-' + tshirtColor[j]]) == 1:
+            images = images + ('|' if j > 0 else '') + df.loc[i]['ArtCode'] + '-TS-' + tshirtColor[j] + '.jpg'
+            images = images + '|' + df.loc[i]['ArtCode'] + '-TS-' + tshirtColor[j] + '-men' + '.jpg'
+            images = images + '|' + df.loc[i]['ArtCode'] + '-TS-' + tshirtColor[j] + '-women' + '.jpg'
+        j += 1
 
+    j = 0
+    for color in hoodieColor:
+        if int(df.loc[i]['Hoodie-' + hoodieColor[j]]) == 1:
+            images = images + '|' + df.loc[i]['ArtCode'] + '-HD-' + hoodieColor[j] + '.jpg'
+            images = images + '|' + df.loc[i]['ArtCode'] + '-HD-' + hoodieColor[j] + '-men' + '.jpg'
+            images = images + '|' + df.loc[i]['ArtCode'] + '-HD-' + hoodieColor[j] + '-women' + '.jpg'
+        j += 1
 
+    pa_style = 'تیشرت|هودی'
+    pa_style_default = 'تیشرت'
+    pa_style_data = '1|1|1'
 
+    pa_sex = 'خانم‌ها|آقایان'
+    pa_sex_default = random.choice(['آقایان', 'خانم‌ها'])
+    pa_sex_data = '2|1|1'
 
+    pa_color = ''
+    j = 0
+    tempColorList = []
+    for color in tshirtColor:
+        if int(df.loc[i]['Tshirt-' + tshirtColor[j]]) == 1:
+            pa_color = pa_color + ('|' if j > 0 else '') + colorIndex[tshirtColor[j]]
+            tempColorList.append(colorIndex[tshirtColor[j]])
+        j += 1
+
+    pa_color_default = random.choice(tempColorList)
+    pa_color_data = '3|1|1'
+
+    pa_size = ''
+    j = 0
+    for size in tshirtSize:
+        pa_size = pa_size + ('|' if j > 0 else '') + tshirtSize[j]
+        j += 1
+    pa_size_default = tshirtSize[1]
+    pa_size_data = '4|1|1'
+
+    pa_material = '۱۰۰ درصد نخ پنبه'
+    pa_material_data = '5|1|0'
+
+    pa_print = 'چاپ دیجیتال با دوام جوهرافشان با استفاده از تکنولوژی مدرن DTG'
+    pa_print_data = '6|1|0'
+
+    pa_wash = 'جهت شست و شو لباس را پشت و رو کرده و با آب زیر ۴۰ درجه بشویید، ترجیحا از پودر صابون جهت شست‌ و شو استفاده کنید'
+    pa_wash_data = '7|1|0'
+
+    pa_default_style = 'تیشرت'
+    pa_default_style_data = '8|1|0'
+
+    wc_average_rating = random.randint(40, 50) / 10
+    wc_rating_count = random.randint(2, 10)
+    wc_review_count = wc_rating_count
+
+    post_content = "<img src='https://artshirt.ir/wp-content/uploads/sizechart/TS-Size-Chart.jpg' style='display: block;margin-left: auto;margin-right: auto;'/>"
+    post_excerpt = "<ul><li><span style='font-size: 12px; color: #808080;'>جنس  ۱۰۰ درصد نخ پنبه</span></li><li><span style='font-size: 12px; color: #808080;'>چاپ دیجیتال با دوام با استفاده از تکنولوژی مدرن DTG</span></li></ul>"
+
+    row = [sku, sku, post_title, post_status, post_type, post_type, product_type, product_cat,
+           product_tag, regular_price, '', regular_price, manage_stock, stock_status, images,
+           pa_style, pa_style_default, pa_style_data, pa_sex, pa_sex_default, pa_sex_data,
+           pa_color, pa_color_default, pa_color_data, pa_size, pa_size_default, pa_size_data,
+           pa_material, pa_material_data, pa_print, pa_print_data, pa_wash, pa_wash_data,
+           pa_default_style, pa_default_style_data, wc_average_rating, wc_rating_count,
+           wc_review_count, stock_status, post_content, post_excerpt
+           ]
+
+    fileName = 'csvfile-TS-' + str(divmod(i, chunkSize)[0]) + '.csv'
+    with open(fileName, "a", newline='') as file:
+        wr = csv.writer(file)
+        if divmod(i, chunkSize)[1] == 0:
+            wr.writerow(mainCol)
+        wr.writerow(row)
+    i += 1
+
+i = 0
+for artCode in df['ArtCode']:
+    # create Hoodie
+    sku = df.loc[i]['ArtCode'] + '-HD'
+    post_title = df.loc[i]['Title']
+    post_status = 'publish'
+    post_type = 'product'
+    product_type = 'variable'
+    product_cat = df.loc[i]['Category']
+    product_cat = product_cat.replace(" - ", ">")
+    product_cat = product_cat.replace("- ", ">")
+    product_cat = product_cat.replace(" -", ">")
+    product_cat = product_cat.replace("-", ">")
+    product_tag = df.loc[i]['Hashtag']
+    regular_price = 120000
+    manage_stock = 'no'
+    stock_status = 'instock'
+
+    j = 0
+    images = ""
+    for color in tshirtColor:
+        if int(df.loc[i]['Tshirt-' + tshirtColor[j]]) == 1:
+            images = images + ('|' if j > 0 else '') + df.loc[i]['ArtCode'] + '-TS-' + tshirtColor[j] + '.jpg'
+            images = images + '|' + df.loc[i]['ArtCode'] + '-TS-' + tshirtColor[j] + '-men' + '.jpg'
+            images = images + '|' + df.loc[i]['ArtCode'] + '-TS-' + tshirtColor[j] + '-women' + '.jpg'
+        j += 1
+
+    j = 0
+    for color in hoodieColor:
+        if int(df.loc[i]['Hoodie-' + hoodieColor[j]]) == 1:
+            images = images + '|' + df.loc[i]['ArtCode'] + '-HD-' + hoodieColor[j] + '.jpg'
+            images = images + '|' + df.loc[i]['ArtCode'] + '-HD-' + hoodieColor[j] + '-men' + '.jpg'
+            images = images + '|' + df.loc[i]['ArtCode'] + '-HD-' + hoodieColor[j] + '-women' + '.jpg'
+        j += 1
+
+    pa_style = 'تیشرت|هودی'
+    pa_style_default = 'هودی'
+    pa_style_data = '1|1|1'
+
+    pa_sex = 'خانم‌ها|آقایان'
+    pa_sex_default = random.choice(['آقایان', 'خانم‌ها'])
+    pa_sex_data = '2|1|1'
+
+    pa_color = ''
+    j = 0
+    tempColorList = []
+    for color in hoodieColor:
+        if int(df.loc[i]['Hoodie-' + hoodieColor[j]]) == 1:
+            pa_color = pa_color + ('|' if j > 0 else '') + colorIndex[hoodieColor[j]]
+            tempColorList.append(colorIndex[hoodieColor[j]])
+        j += 1
+
+    pa_color_default = random.choice(tempColorList)
+    pa_color_data = '3|1|1'
+
+    pa_size = ''
+    j = 0
+    for size in hoodieSize:
+        pa_size = pa_size + ('|' if j > 0 else '') + hoodieSize[j]
+        j += 1
+    pa_size_default = hoodieSize[1]
+    pa_size_data = '4|1|1'
+
+    pa_material = '۱۰۰ درصد نخ پنبه'
+    pa_material_data = '5|1|0'
+
+    pa_print = 'چاپ دیجیتال با دوام جوهرافشان با استفاده از تکنولوژی مدرن DTG'
+    pa_print_data = '6|1|0'
+
+    pa_wash = 'جهت شست و شو لباس را پشت و رو کرده و با آب زیر ۴۰ درجه بشویید، ترجیحا از پودر صابون جهت شست‌ و شو استفاده کنید'
+    pa_wash_data = '7|1|0'
+
+    pa_default_style = 'هودی'
+    pa_default_style_data = '8|1|0'
+
+    wc_average_rating = random.randint(40, 50) / 10
+    wc_rating_count = random.randint(2, 10)
+    wc_review_count = wc_rating_count
+
+    post_content = "<img src='https://artshirt.ir/wp-content/uploads/sizechart/TS-Size-Chart.jpg' style='display: block;margin-left: auto;margin-right: auto;'/>"
+    post_excerpt = "<ul><li><span style='font-size: 12px; color: #808080;'>جنس ۱۰۰ درصد نخ پنبه</span></li><li><span style='font-size: 12px; color: #808080;'>چاپ دیجیتال با دوام با استفاده از تکنولوژی مدرن DTG</span></li></ul>"
+
+    row = [sku, sku, post_title, post_status, post_type, post_type, product_type, product_cat,
+           product_tag, regular_price, '', regular_price, manage_stock, stock_status, images,
+           pa_style, pa_style_default, pa_style_data, pa_sex, pa_sex_default, pa_sex_data,
+           pa_color, pa_color_default, pa_color_data, pa_size, pa_size_default, pa_size_data,
+           pa_material, pa_material_data, pa_print, pa_print_data, pa_wash, pa_wash_data,
+           pa_default_style, pa_default_style_data, wc_average_rating, wc_rating_count,
+           wc_review_count, stock_status, post_content, post_excerpt
+           ]
+
+    fileName = 'csvfile-HD-' + str(divmod(i, chunkSize)[0]) + '.csv'
+    with open(fileName, "a", newline='') as file:
+        wr = csv.writer(file)
+        if divmod(i, chunkSize)[1] == 0:
+            wr.writerow(mainCol)
+        wr.writerow(row)
     i += 1
